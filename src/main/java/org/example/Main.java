a6f29c0de60ac416b9c8390539ed9f917ce920b9
lenaruppert <l.zi@gmx.de>
2023-01-26 16:16:16 +0100

    print Test Lena

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,6 +7,7 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Hi");
         System.out.println("Anas Line");
+        System.out.println("Test Lena");
     }
 
 
